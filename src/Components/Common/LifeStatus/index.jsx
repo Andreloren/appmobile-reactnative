c9bbc79389da990e9b96bcafdf0bb78fb8d8830b
Andreloren <andre.lorente83@gmail.com>
2022-12-05 11:20:28 -0300

    Estruturando as Animações

--- src/Components/Common/LifeStatus/index.jsx
+++ src/Components/Common/LifeStatus/index.jsx
@@ -1,29 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, StyleSheet } from "react-native";
 
 import Lottie from "lottie-react-native";
 
-export default function LifeStatus() {
+import AnimationService from "../../../Services/AnimationService";
+
+export default function LifeStatus({
+  mindHabit,
+  moneyHabit,
+  bodyHabit,
+  funHabit,
+}) {
+  const [mind, setMind] = useState();
+  const [money, setMoney] = useState();
+  const [robot, setRobot] = useState();
+
+  useEffect(() => {
+    AnimationService.animationStatus(
+      mindHabit?.progressBar,
+      moneyHabit?.progressBar,
+      bodyHabit?.progressBar,
+      funHabit?.progressBar,
+      setMind,
+      setMoney,
+      setRobot
+    );
+  }, [mindHabit, moneyHabit, bodyHabit, funHabit]);
+
   return (
     <View style={styles.container}>
-      <Lottie
-        source={require("../../../assets/education/education-100.json")}
-        autoPlay
-        loop
-        style={styles.educacaoAnimacao}
-      />
-      <Lottie
-        source={require("../../../assets/money/money-100.json")}
-        autoPlay
-        loop
-        style={styles.financasAnimacao}
-      />
-      <Lottie
-        source={require("../../../assets/robot/robot-100-100.json")}
-        autoPlay
-        loop
-        style={styles.roboAnimacao}
-      />
+      <Lottie source={mind} autoPlay loop style={styles.educacaoAnimacao} />
+      <Lottie source={money} autoPlay loop style={styles.financasAnimacao} />
+      <Lottie source={robot} autoPlay loop style={styles.roboAnimacao} />
     </View>
   );
 }
